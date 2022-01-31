f6142c397e7c9cbaee923077b809de70335b676a
AlonaPiasetskaDev <alona.piasetska.dev@gmail.com>
2022-01-31 11:02:12 +0200

    prepare for integrate ngRx store

--- src/app/components/admin/children/profiles-list/profiles-list.component.ts
+++ src/app/components/admin/children/profiles-list/profiles-list.component.ts
@@ -14,11 +14,28 @@ export class ProfilesListComponent implements OnInit {
 
   constructor(private activatedRoute: ActivatedRoute, private ps: ProfilesService) { }
   profiles: any = [];
+  name: string;
+  username: string;
+
+  isEditing = false;
+
+  openAddProfileModal(){
+    console.log("add profile")
+  }
+
+  editProfile($event: Event){
+    this.isEditing = true;
+    $event.stopPropagation();
+    console.log('edit profile');
+
+  }
+
+  saveChanges(){
+    console.log('saved!')
+  }
 
   ngOnInit(): void {
-    console.log(this.activatedRoute.snapshot);
     this.ps.getProfiles().subscribe((data) => {
-      console.log(data);
       this.profiles = data;
     });
   }
--- src/app/components/admin/children/profiles-list/profiles-list.module.ts
+++ src/app/components/admin/children/profiles-list/profiles-list.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 
 import { HttpClientModule } from '@angular/common/http';
 import { ProfilesListComponent } from './profiles-list.component';
+import { AddProfileComponent } from './add-profile/add-profile/add-profile.component';
 
 
 @NgModule({
@@ -9,5 +10,8 @@ import { ProfilesListComponent } from './profiles-list.component';
     HttpClientModule
   ],
   bootstrap: [ProfilesListComponent],
+  declarations: [
+    AddProfileComponent
+  ],
 })
 export class AdminModule { }
